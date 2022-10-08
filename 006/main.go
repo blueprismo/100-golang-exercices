809f3404030086a403b38fd2848d0da4c6cdd990
blueprismo <eninkadukk@gmail.com>
2022-10-08 23:27:00 +0200

    Add exercise 6

---
+++ 006/main.go
@@ -0,0 +1,24 @@
+// Exercise: Learn FORMAT STRING!! Print some verbs with their corresponding letter
+// Format string / verbs are followed by a '%' character.
+// Example: fmt.Printf("Hello, my name is %s", name) // will substitute %s for the content in the variable name called "name"
+
+// Tip: You can head into the documentation for reference: https://pkg.go.dev/fmt#hdr-Printing
+
+package main
+
+import "fmt"
+
+func main () {
+	var name string
+	var age	int64
+	var legal bool
+	var weight float32
+
+	name = "Anna"
+	age  = 20
+	legal = false
+	weight = 70.12
+
+	// Here goes your code
+	fmt.Printf("...")
+}

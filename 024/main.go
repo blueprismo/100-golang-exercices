54a60a50f1184c167f18cba0c68c14879c9fb395
blueprismo <eninkadukk@gmail.com>
2022-10-09 19:07:27 +0200

    Exercise 24

---
+++ 024/main.go
@@ -0,0 +1,12 @@
+// Exercise: Pointers
+
+// Print the address of a variable
+package main
+
+import "fmt"
+
+func main () {
+    var x := 5
+	// Your code goes here
+	
+}
---
+++ 024/solution/solution.go
@@ -0,0 +1,12 @@
+// Exercise: Pointers
+
+// Print the address of a variable
+package main
+
+import "fmt"
+
+func main () {
+    var x int = 5
+	// Your code goes here
+	fmt.Println(&x)
+}

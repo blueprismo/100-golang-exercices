a7fbc05fbe273eeb497a5e56636d63f631bf81ad
blueprismo <eninkadukk@gmail.com>
2022-10-09 10:41:03 +0200

    Exercise 9

---
+++ 009/main.go
@@ -0,0 +1,11 @@
+// Exercise: Arrays
+// Create an array of 10 "int8" values, in it's initialization, fill those values from 0 to 9
+
+package main
+
+import "fmt"
+
+func main () {
+	// Here goes your code
+	fmt.Printf("...")
+}

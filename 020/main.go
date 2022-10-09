232df75bd0133116232ebde7b237ce352cd3d82c
blueprismo <eninkadukk@gmail.com>
2022-10-09 18:46:41 +0200

    Exercise 20

---
+++ 020/main.go
@@ -0,0 +1,21 @@
+// Exercise: STRUCT
+// Create a Hotel structure with:
+// numRooms int32
+// streetName string
+// hasPool bool
+
+// Then assign a value to each of those attributes
+
+package main
+
+import "fmt"
+
+type Hotel struct {
+	// Your code goes here
+
+}
+
+func main () {
+	var myHotel Hotel
+	// Your code goes here
+}

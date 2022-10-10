6ff7bb14aae9db3002fad9aae4c6b8149fa592de
Enin Kaduk <eninkadukk@gmail.com>
2022-10-10 16:20:37 +0200

    Exercise 36

--- 036/main.go
+++ 036/main.go
@@ -1,6 +1,7 @@
 // Exercise: Channels synchronisation
 
-// We have a goroutine task, learn how the channel synchronisation works and make the program wait for the asynchronous function (make it synchronous ;) )
+// There are two goroutines now
+// Call the first goroutine (named 'task') and run the second goroutine 'task2' ONLY after the first one has finished
 
 package main
 
@@ -8,18 +9,19 @@ import "fmt"
 import "time"
 
 func task(done chan bool) {
-    fmt.Print("running...")
+    fmt.Print("running Task1 goroutine...")
     time.Sleep(time.Second)
     fmt.Println("done")
-
     done <- true
 }
 
-func main () {
-	var done chan bool = make(chan bool)	
-	go task(done)
+func task2(){
+	fmt.Println("Task2 goroutine...")
+}
 
-	// What would happen if we comment out this next line "<- done"?
+func main () {
+	var done chan bool = make(chan bool,1)	
+	fmt.Println("I am running in the main thread concurrently")
 	// Your code goes here
-	<- done
+	
 }

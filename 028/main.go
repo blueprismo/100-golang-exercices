15472d33666ed3c746600bc7154b3a61fb171240
Enin Kaduk <eninkadukk@gmail.com>
2022-10-10 13:11:51 +0200

    Exercise 28

---
+++ 028/main.go
@@ -0,0 +1,23 @@
+// Exercise: Functions
+
+// Create a function that sums 2 numbers, then nest that function in a second function, that will add a third number
+// In the main program, only call the secondsum function
+
+package main
+
+import "fmt"
+
+func sum(x int, y int) int {
+	
+}
+
+func secondSum(x, y, z int) int {
+	
+}
+
+func main () {
+	var result int
+	// Your code goes here
+
+	fmt.Println(result)
+}

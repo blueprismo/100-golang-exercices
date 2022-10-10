721d9c5281642caa8900ae72e16a5b90b1c28bd9
Enin Kaduk <eninkadukk@gmail.com>
2022-10-10 16:29:25 +0200

    Exercise 37

---
+++ 037/solution/solution.go
@@ -0,0 +1,21 @@
+// Exercise: Channels directions (only read/rx)
+
+// Make a goroutine with a channel for only receive data.
+// The function should be called "receive" and the receive-only channel should be it's 1st and only argument
+// Sending data from that channel is prohibited / will cause compiler errors
+// Feed some string into that channel.
+
+
+package main
+
+import "fmt"
+
+func receive(c <- chan string){
+	fmt.Println(<-c)
+}
+
+func main () {
+	var c chan string = make(chan string, 1)
+	c <- "HelloWorld!"
+	receive(c)
+}

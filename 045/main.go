8fbdfac64ec9e0e2d7c0c02410a73734ddcb33fb
Enin Kaduk <eninkadukk@gmail.com>
2022-10-12 10:25:54 +0200

    Exercise 45

--- 045/main.go
+++ 045/main.go
@@ -1,7 +1,7 @@
 // Exercise: Tickers
 
 // Create a goroutine with a infinite loop
-// The condition for that infinite loop will be a range that goes over the "time.Tick(time.Second * 1)"
+// The condition for that infinite loop will be a range that goes over the "time.Tick(time.Second * 1)", in other words: a simple ticker
 // And at every Tick, we will print "Tick"
 // In the main function, call the go routine :)
 

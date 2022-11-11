c0f9fd24433364afba5b933e4c5e9de97ede2508
Blueprismo <enin.kaduk@xentral.com>
2022-11-11 21:36:53 +0100

    Exercise 70

---
+++ 070/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+// import the "testing" library
+import (
+    "testing"
+)
+
+// In the TestReverse function, create a struct called 'testcases' containing 2 variables "in" and "want" of type string
+
+func TestReverse(t *testing.T) {
+    testcases := []struct {
+    
+    }{ 
+		// populate 3 cases of strings in the "in" variable, and reverse it in the "want" variable
+    }
+	// iterate over the testcases and test if the Reversed string is equal to want. If its not raise an error
+    
+}

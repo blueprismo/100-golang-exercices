d3c41d2e659675c6340684ed7edf9b29d781be9c
Blueprismo <enin.kaduk@xentral.com>
2022-11-11 21:17:49 +0100

    Exercise 68

---
+++ 068/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+// Testing!! 
+// In this file we are going to create the beforementioned test function! (read the main.go file first!!)
+
+import "fmt"
+// Import the "testing" package here
+
+
+
+// Create the test function called TestHello with the signature (t *testing.T)
+func  {
+	// Inside this function, create 2 variables:
+	// "have" with the value as the function HelloTest()
+	
+	// "want" with the hardcoded string "Hello, Test"
+	
+
+	// conditional: if have != want, raise t.Error() with a message
+	
+	// else print Test passed!
+	
+}
+
+// now run the command "go test" in this directory and see what happens :) 

1051ccf742d3356bb2b5cd3dcffc640f53fb433d
Blueprismo <enin.kaduk@xentral.com>
2022-12-04 14:59:51 +0100

    Exercise 75

--- 075/solution/solution.go
+++ 075/solution/solution.go
@@ -11,16 +11,37 @@ package main
 // 2- Once installed, create a file named ".env" containing your 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	//"encoding/json"
+	//"fmt"
 	"log"
 	"os"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
+	//"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main (){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load .env file")
+	}
 
+	// Create a variable named "uri" containing your MONGODB_URI string connection.
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	log.Println("You got connected!")
 }

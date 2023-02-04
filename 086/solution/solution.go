a818fd046b2a9ca2da7968ee8ff2ad7526abd615
Blueprismo <enin.kaduk@xentral.com>
2023-02-04 22:07:07 +0100

    Exercise 86

--- 086/solution/solution.go
+++ 086/solution/solution.go
@@ -1,26 +1,23 @@
-// PROTOBUFFS!
-// What is a protocol buffer?
-// Please take your time to read, soak and understand a bit what is protobuff
-// https://developers.google.com/protocol-buffers
-// https://en.wikipedia.org/wiki/Protocol_Buffers
-// https://github.com/protocolbuffers/protobuf
-// https://protobuf.dev/
-// Wow! Much doc!
+// Contexts
+// I feel contexts are a really powerful feature in GO and I haven't given the importance that it deserves.
 
-// Once you have read this, we are going to begin this exercise. The goal will be declaring a `.proto` file
+// In this episodes, we are going to talk about contexts!
+package main
 
-// 1- Install go dependencies for protocol buffers:
-// go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+import (
+	"context" // import the "context" package
+	"fmt"
+)
 
-// 2- Set your Gopath bin into your path like this:
-// export PATH="$PATH:$(go env GOPATH)/bin"
-// 2.1- Ensure you have protoc-gen-go by issuing:
-// protoc-gen-go --version
+// Create a function called doSomething with an argument ctx of type context.Context (this is an interface)
+func doSomething(ctx context.Context) {
+	fmt.Println("Doing something!")
+}
 
-// 3- Create a message inside the "person.proto" file
+func main() {
+	// create a variable called ctx, this variable will have a context.TODO() value, which returns an empty Context.
+	ctx := context.TODO()
 
-// 4- That message will be called Person, and it will have 2 fields: 
-// 4.1- "name" a string with the tag number '1'
-// 4.2- "age" an int32 with the tag number '2'
-
-// That's it! This is what you need to complete this exercise.
+	// call the function with the empty context created before as it's only argument
+	doSomething(ctx)
+}

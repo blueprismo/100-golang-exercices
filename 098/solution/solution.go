6f3d456d500e9e3c17a49ae4420c5b5f4480e65f
Blueprismo <enin.kaduk@xentral.com>
2023-02-05 12:54:28 +0100

    Exercise 98

---
+++ 098/solution/solution.go
@@ -0,0 +1,23 @@
+// Interfaces -  Empty interfaces, bear in mind...
+package main
+
+import (
+	"fmt"
+)
+
+type human map[string]interface{}
+
+// But there's an important thing to point out when it comes to retrieving and using a value from this map
+// let's say that we want to get the "age" value and increment it by 1.
+func main() {
+	person := make(human)
+	person["name"] = "Alice"
+    person["age"] = 21
+    person["height"] = 167.64
+
+	// Try to update the person age, the new value will be the existing one plus 1
+	// (When you run the program, see what happens), what's the error it arises?
+	person["age"] = person["age"] + 1
+
+    fmt.Printf("%+v", person)
+}

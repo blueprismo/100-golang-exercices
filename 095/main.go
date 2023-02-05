9dbd0447dac394866c87dc1fcb5a69e52cc767cf
Blueprismo <enin.kaduk@xentral.com>
2023-02-05 12:19:15 +0100

    Exercise 95

---
+++ 095/main.go
@@ -0,0 +1,37 @@
+// Interfaces - Intro - Implicit implementation
+
+// In golang, we don't use the "implements" keyword when using an interface.
+// We don't have to say that explicitly, instead we need some type to satisfy the area() method.
+// We say something implements an interface if it has a method with the exact signature.
+// Inside our geometry interface, we have a signature called "area()"
+// In this exercise, we are going to create a function with that signature!
+
+package main
+
+import (
+	"fmt"
+)
+
+type geometry interface{
+	area() float64
+}
+
+// A rectangle struct
+type rect struct {
+    width, height float64
+}
+
+// We are going to create a function with the area() signature.
+// The function will be a receiver function (the receiver will be a rectangle "r")
+// The function will be named area, without any arguments and will return a float64 value
+
+	// The return value will be the area of the rectangle
+
+
+
+func main() {
+	// Create a new rectangle and store it in a variable
+	
+	// Print the variable
+	fmt.Println(r.area())
+}

543733193fa783b0f6f428966882454a090d85a4
Blueprismo <enin.kaduk@xentral.com>
2023-02-05 11:58:29 +0100

    Exercise 94

---
+++ 094/main.go
@@ -0,0 +1,29 @@
+// Interfaces - Intro
+
+// An interface type in Go is kind of like a definition. It defines and describes the exact methods that some other type must have.
+// Interfaces are a concept that at first may need some time to be understood, take all your time.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// This first exercise we are going to create an interface, no execution!
+// Creating our first interface:
+// declare an interface called "Geometry"
+
+	// create a function signature area(), it's return type will be float64
+	
+
+
+// A rectangle struct
+type rect struct {
+    width, height float64
+}
+
+func main() {
+	ctx := context.Background()
+	doSomething(ctx)
+}
